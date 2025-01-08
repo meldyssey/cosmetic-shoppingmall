ce77dcff37337022c4d53a8e514531ca39bff6b2
meldyssey <meldyssey15@gmail.com>
2025-01-08 10:55:56 +0900

    1/8 배송완료 업데이트 코드 추가

--- frontend/src/components/mypage/Navbar.js
+++ frontend/src/components/mypage/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "../../scss/mypage/Navbar.module.scss";
+import axios from "axios";
+
+const bkURL = process.env.REACT_APP_BACK_URL;
 
 function Navbar() {
     const navigate = useNavigate();
     const customerName = sessionStorage.getItem("customerName"); // 세션에서 이름 가져오기
+    const email = sessionStorage.getItem("email");
 
     const Logout = (e) => {
         e.preventDefault(); // Link의 기본 이동 동작 방지
@@ -17,7 +21,8 @@ function Navbar() {
         }
     };
 
-    const updateDelivery = () => {
+    const updateDelivery = (e) => {
+        e.preventDefault(); // Link의 기본 이동 동작 방지
         axios
             .post(`${bkURL}/delivery/update`, { email })
             .then(() => {
@@ -26,6 +31,8 @@ function Navbar() {
             .catch((error) => {
                 console.error("배송 완료 업데이트실패:", error);
             });
+
+        navigate("viewOrders"); // 배송주소록으로 이동
     };
 
     return (
@@ -39,10 +46,10 @@ function Navbar() {
                 {/* 중앙 링크 */}
                 <div className={styles.centerLinks}>
                     <Link to="myinfoEdit">회원정보 수정</Link>
-                    <Link to="addressList" onClick={updateDelivery}>
-                        배송 주소록
+                    <Link to="addressList">배송 주소록</Link>
+                    <Link to="viewOrders" onClick={updateDelivery}>
+                        주문내역 보기
                     </Link>
-                    <Link to="viewOrders">주문내역 보기</Link>
                     <Link to="/onetoonelist">1:1 문의</Link>
                 </div>
 
